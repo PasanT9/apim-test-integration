0d2bdf2fa720b93bd59943b1e94db599aebf88c7
pasant9 <pasan96tennakoon@gmail.com>
2024-03-06 17:55:04 +0530

    Add testng

--- run-int-test.sh
+++ run-int-test.sh
@@ -245,4 +245,7 @@ pwd
 log_info "install pack into local maven Repository"
 mvn install:install-file -Dfile=/opt/testgrid/workspace/product-apim/modules/distribution/product/target/wso2am-4.3.0-SNAPSHOT.zip -DgroupId=org.wso2.am -DartifactId=wso2am -Dversion=4.3.0-SNAPSHOT -Dpackaging=zip
 install_jdk21
-cd $INT_TEST_MODULE_DIR  && mvn clean install -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Ptestgrid -DskipBenchMarkTest=true -Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false
+cd $INT_TEST_MODULE_DIR
+rm -rf tests-integration/tests-backend/src/test/resources/testng.xml
+curl -o tests-integration/tests-backend/src/test/resources/testng.xml https://raw.githubusercontent.com/PasanT9/apim-test-integration/4.3.0-jdk/testng.xml
+mvn clean install -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Ptestgrid -DskipBenchMarkTest=true -Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false
