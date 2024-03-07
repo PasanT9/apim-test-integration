829d6d0834cd27361014f822dc77471b1700a98a
pasant9 <pasan96tennakoon@gmail.com>
2024-03-07 15:56:30 +0530

    Add infra.json file

--- run-int-test.sh
+++ run-int-test.sh
@@ -72,15 +72,15 @@ function export_db_params(){
     db_name=$1
 
     export SHARED_DATABASE_DRIVER=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .driver' ${INFRA_JSON})
-    export SHARED_DATABASE_URL=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_COMMON_DB") | .url' ${INFRA_JSON})
-    export SHARED_DATABASE_USERNAME=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_COMMON_DB") | .username' ${INFRA_JSON})
-    export SHARED_DATABASE_PASSWORD=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_COMMON_DB") | .password' ${INFRA_JSON})
+    export SHARED_DATABASE_URL=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_SHARED_DB") | .url' ${INFRA_JSON})
+    export SHARED_DATABASE_USERNAME=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_SHARED_DB") | .username' ${INFRA_JSON})
+    export SHARED_DATABASE_PASSWORD=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_SHARED_DB") | .password' ${INFRA_JSON})
     export SHARED_DATABASE_VALIDATION_QUERY=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .validation_query' ${INFRA_JSON})
     
     export API_MANAGER_DATABASE_DRIVER=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .driver' ${INFRA_JSON})
-    export API_MANAGER_DATABASE_URL=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_APIMGT_DB") | .url' ${INFRA_JSON})
-    export API_MANAGER_DATABASE_USERNAME=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_APIMGT_DB") | .username' ${INFRA_JSON})
-    export API_MANAGER_DATABASE_PASSWORD=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_APIMGT_DB") | .password' ${INFRA_JSON})
+    export API_MANAGER_DATABASE_URL=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_APIM_DB") | .url' ${INFRA_JSON})
+    export API_MANAGER_DATABASE_USERNAME=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_APIM_DB") | .username' ${INFRA_JSON})
+    export API_MANAGER_DATABASE_PASSWORD=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .database[] | select ( .name == "WSO2AM_APIM_DB") | .password' ${INFRA_JSON})
     export API_MANAGER_DATABASE_VALIDATION_QUERY=$(jq -r '.jdbc[] | select ( .name == '\"${db_name}\"' ) | .validation_query' ${INFRA_JSON})
 }
 
@@ -106,7 +106,8 @@ cd modules/distribution/product/target/
 unzip wso2am-4.3.0-SNAPSHOT.zip
 rm -rf wso2am-4.3.0-SNAPSHOT.zip
 
-wget https://raw.githubusercontent.com/wso2/testgrid/5c8de3cedc932e1753bb2c5e47e7d3af2ff19535/jobs/intg-test-resources/infra.json
+
+wget https://raw.githubusercontent.com/PasanT9/apim-test-integration/4.3.0-copy/infra.json
 
 db_file=$(jq -r '.jdbc[] | select ( .name == '\"${DB_TYPE}\"') | .file_name' ${INFRA_JSON})
 wget -q https://integration-testgrid-resources.s3.amazonaws.com/lib/jdbc/${db_file}.jar  -P /opt/testgrid/workspace/product-apim/modules/distribution/product/target/wso2am-4.3.0-SNAPSHOT/repository/components/lib/
@@ -140,22 +141,22 @@ if [[ $DB_TYPE = "mysql" ]]; then
     log_info "Mysql DB is selected! Running mysql scripts for apim $WSO2_PRODUCT_VERSION"
     # create databases
     log_info "[Mysql] Droping Databases if exist"
-    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "DROP DATABASE IF EXISTS WSO2AM_COMMON_DB"
-    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "DROP DATABASE IF EXISTS WSO2AM_APIMGT_DB"
+    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "DROP DATABASE IF EXISTS WSO2AM_SHARED_DB"
+    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "DROP DATABASE IF EXISTS WSO2AM_APIM_DB"
     mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "DROP DATABASE IF EXISTS WSO2AM_STAT_DB"
 
     log_info "[Mysql] Creating Databases"
-    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "CREATE DATABASE WSO2AM_COMMON_DB CHARACTER SET latin1"
-    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "CREATE DATABASE WSO2AM_APIMGT_DB CHARACTER SET latin1"
+    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "CREATE DATABASE WSO2AM_SHARED_DB CHARACTER SET latin1"
+    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "CREATE DATABASE WSO2AM_APIM_DB CHARACTER SET latin1"
     mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -e "CREATE DATABASE WSO2AM_STAT_DB CHARACTER SET latin1"
 
-    log_info "[Mysql] Povisioning WSO2AM_APIMGT_DB"
-    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -D WSO2AM_APIMGT_DB <  $DB_SCRIPT_PATH/apimgt/mysql.sql
-    log_info "End [Mysql] Povisioning WSO2AM_APIMGT_DB"
+    log_info "[Mysql] Povisioning WSO2AM_APIM_DB"
+    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -D WSO2AM_APIM_DB <  $DB_SCRIPT_PATH/apimgt/mysql.sql
+    log_info "End [Mysql] Povisioning WSO2AM_APIM_DB"
 
-    log_info "[Mysql] Povisioning WSO2AM_COMMON_DB"
-    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -D WSO2AM_COMMON_DB <  $DB_SCRIPT_PATH/mysql.sql
-    log_info "End [Mysql] Povisioning WSO2AM_COMMON_DB"
+    log_info "[Mysql] Povisioning WSO2AM_SHARED_DB"
+    mysql -u $CF_DB_USERNAME -p$CF_DB_PASSWORD -h $CF_DB_HOST -P $CF_DB_PORT -D WSO2AM_SHARED_DB <  $DB_SCRIPT_PATH/mysql.sql
+    log_info "End [Mysql] Povisioning WSO2AM_SHARED_DB"
 
 elif [[ $DB_TYPE = "postgres" ]]; then  
 
@@ -163,75 +164,75 @@ elif [[ $DB_TYPE = "postgres" ]]; then
     export PGPASSWORD="$CF_DB_PASSWORD"
     
     log_info "[Postgres] Droping Databases if exist"
-    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "DROP DATABASE IF EXISTS \"WSO2AM_COMMON_DB\""
-    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "DROP DATABASE IF EXISTS \"WSO2AM_APIMGT_DB\""
+    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "DROP DATABASE IF EXISTS \"WSO2AM_SHARED_DB\""
+    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "DROP DATABASE IF EXISTS \"WSO2AM_APIM_DB\""
     psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "DROP DATABASE IF EXISTS \"WSO2AM_STAT_DB\""
 
     log_info "[Postgres] Creating databases"
-    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "CREATE DATABASE \"WSO2AM_COMMON_DB\""
-    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "CREATE DATABASE \"WSO2AM_APIMGT_DB\""
+    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "CREATE DATABASE \"WSO2AM_SHARED_DB\""
+    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "CREATE DATABASE \"WSO2AM_APIM_DB\""
     psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d postgres -c "CREATE DATABASE \"WSO2AM_STAT_DB\""
 
-    log_info "[Postgres] Provisioning database WSO2AM_APIMGT_DB"
-    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d WSO2AM_APIMGT_DB -f $DB_SCRIPT_PATH/apimgt/postgresql.sql
-    log_info "[Postgres] Provisioning database WSO2AM_COMMON_DB"
-    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d WSO2AM_COMMON_DB -f $DB_SCRIPT_PATH/postgresql.sql
+    log_info "[Postgres] Provisioning database WSO2AM_APIM_DB"
+    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d WSO2AM_APIM_DB -f $DB_SCRIPT_PATH/apimgt/postgresql.sql
+    log_info "[Postgres] Provisioning database WSO2AM_SHARED_DB"
+    psql -U $CF_DB_USERNAME -h $CF_DB_HOST -p $CF_DB_PORT -d WSO2AM_SHARED_DB -f $DB_SCRIPT_PATH/postgresql.sql
 
 elif [[ $DB_TYPE =~ "oracle-se" ]]; then
-	
+	echo "This is the new script"
     export ORACLE_HOME=/usr/lib/oracle/12.2/client64/
     export PATH=$PATH:/usr/lib/oracle/12.2/client64/bin/
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ORACLE_HOME/lib:$ORACLE_HOME
 
     log_info "Oracle DB is selected! Running Oracle scripts for apim $WSO2_PRODUCT_VERSION"
     # Create users to the required DB
-    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2AM_APIMGT_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2AM_APIMGT_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" > apim_oracle_user.sql
-    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2AM_COMMON_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2AM_COMMON_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> apim_oracle_user.sql
+    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2AM_APIM_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2AM_APIM_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" > apim_oracle_user.sql
+    echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2AM_SHARED_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2AM_SHARED_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> apim_oracle_user.sql
     echo "DECLARE USER_EXIST INTEGER;"$'\n'"BEGIN SELECT COUNT(*) INTO USER_EXIST FROM dba_users WHERE username='WSO2AM_STAT_DB';"$'\n'"IF (USER_EXIST > 0) THEN EXECUTE IMMEDIATE 'DROP USER WSO2AM_STAT_DB CASCADE';"$'\n'"END IF;"$'\n'"END;"$'\n'"/" >> apim_oracle_user.sql
-    echo "CREATE USER WSO2AM_COMMON_DB IDENTIFIED BY $CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2AM_COMMON_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2AM_COMMON_DB;" >> apim_oracle_user.sql
-    echo "CREATE USER WSO2AM_APIMGT_DB IDENTIFIED BY $CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2AM_APIMGT_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2AM_APIMGT_DB;" >> apim_oracle_user.sql
+    echo "CREATE USER WSO2AM_SHARED_DB IDENTIFIED BY $CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2AM_SHARED_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2AM_SHARED_DB;" >> apim_oracle_user.sql
+    echo "CREATE USER WSO2AM_APIM_DB IDENTIFIED BY $CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2AM_APIM_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2AM_APIM_DB;" >> apim_oracle_user.sql
     echo "CREATE USER WSO2AM_STAT_DB IDENTIFIED BY $CF_DB_PASSWORD;"$'\n'"GRANT CONNECT, RESOURCE, DBA TO WSO2AM_STAT_DB;"$'\n'"GRANT UNLIMITED TABLESPACE TO WSO2AM_STAT_DB;" >> apim_oracle_user.sql
     echo "ALTER SYSTEM SET open_cursors = 3000 SCOPE=BOTH;">> apim_oracle_user.sql
     # Create the tables
     log_info "[Oracle] Creating Users"
     echo exit | sqlplus64 '$CF_DB_USERNAME/$CF_DB_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=$CF_DB_HOST)(Port=$CF_DB_PORT))(CONNECT_DATA=(SID=WSO2AMDB)))' @apim_oracle_user.sql
     log_info "[Oracle] Creating Tables"
-    echo exit | sqlplus64 'WSO2AM_COMMON_DB/$CF_DB_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=$CF_DB_HOST)(Port=$CF_DB_PORT))(CONNECT_DATA=(SID=WSO2AMDB)))' @$DB_SCRIPT_PATH/oracle.sql
-    echo exit | sqlplus64 'WSO2AM_APIMGT_DB/$CF_DB_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=$CF_DB_HOST)(Port=$CF_DB_PORT))(CONNECT_DATA=(SID=WSO2AMDB)))' @$DB_SCRIPT_PATH/apimgt/oracle.sql
+    echo exit | sqlplus64 'WSO2AM_SHARED_DB/$CF_DB_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=$CF_DB_HOST)(Port=$CF_DB_PORT))(CONNECT_DATA=(SID=WSO2AMDB)))' @$DB_SCRIPT_PATH/oracle.sql
+    echo exit | sqlplus64 'WSO2AM_APIM_DB/$CF_DB_PASSWORD@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=$CF_DB_HOST)(Port=$CF_DB_PORT))(CONNECT_DATA=(SID=WSO2AMDB)))' @$DB_SCRIPT_PATH/apimgt/oracle.sql
 
 elif [[ $DB_TYPE =~ "sqlserver-se" ]]; then
     log_info "SQL Server DB Engine is selected! Running MSSql scripts for apim $WSO2_PRODUCT_VERSION"
 
     log_info "[SQLServer] Droping Databases if exist"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "DROP DATABASE IF EXISTS WSO2AM_COMMON_DB"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "DROP DATABASE IF EXISTS WSO2AM_APIMGT_DB"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "DROP DATABASE IF EXISTS WSO2AM_SHARED_DB"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "DROP DATABASE IF EXISTS WSO2AM_APIM_DB"
 
     log_info "[SQLServer] Creating Databases"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "CREATE DATABASE WSO2AM_COMMON_DB"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "CREATE DATABASE WSO2AM_APIMGT_DB"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "CREATE DATABASE WSO2AM_SHARED_DB"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "CREATE DATABASE WSO2AM_APIM_DB"
 
-    log_info "[SQLServer] Provisioning database WSO2AM_APIMGT_DB"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -d WSO2AM_APIMGT_DB -i $DB_SCRIPT_PATH/apimgt/mssql.sql
-    log_info "[SQLServer] Provisioning database WSO2AM_COMMON_DB"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -d WSO2AM_COMMON_DB -i $DB_SCRIPT_PATH/mssql.sql
+    log_info "[SQLServer] Provisioning database WSO2AM_APIM_DB"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -d WSO2AM_APIM_DB -i $DB_SCRIPT_PATH/apimgt/mssql.sql
+    log_info "[SQLServer] Provisioning database WSO2AM_SHARED_DB"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -d WSO2AM_SHARED_DB -i $DB_SCRIPT_PATH/mssql.sql
     log_info "[SQLServer] Tuning databases"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_APIMGT_DB  SET ALLOW_SNAPSHOT_ISOLATION ON"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_APIMGT_DB SET READ_COMMITTED_SNAPSHOT ON WITH ROLLBACK IMMEDIATE"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_COMMON_DB  SET ALLOW_SNAPSHOT_ISOLATION ON"
-    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_COMMON_DB SET READ_COMMITTED_SNAPSHOT ON WITH ROLLBACK IMMEDIATE"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_APIM_DB  SET ALLOW_SNAPSHOT_ISOLATION ON"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_APIM_DB SET READ_COMMITTED_SNAPSHOT ON WITH ROLLBACK IMMEDIATE"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_SHARED_DB  SET ALLOW_SNAPSHOT_ISOLATION ON"
+    sqlcmd -S $CF_DB_HOST -U $CF_DB_USERNAME -P $CF_DB_PASSWORD -Q "ALTER DATABASE WSO2AM_SHARED_DB SET READ_COMMITTED_SNAPSHOT ON WITH ROLLBACK IMMEDIATE"
 
 fi
 
-zip -r wso2am-4.3.0-SNAPSHOT.zip wso2am-4.3.0-SNAPSHOT
-rm -rf wso2am-4.3.0-SNAPSHOT
-cd ../../../../
-pwd
+#zip -r wso2am-4.3.0-SNAPSHOT.zip wso2am-4.3.0-SNAPSHOT
+#rm -rf wso2am-4.3.0-SNAPSHOT
+#cd ../../../../
+#pwd
 
 
 # Testing..............................................
-log_info "install pack into local maven Repository"
-mvn install:install-file -Dfile=/opt/testgrid/workspace/product-apim/modules/distribution/product/target/wso2am-4.3.0-SNAPSHOT.zip -DgroupId=org.wso2.am -DartifactId=wso2am -Dversion=4.3.0-SNAPSHOT -Dpackaging=zip
-cd $INT_TEST_MODULE_DIR
-rm -rf tests-integration/tests-backend/src/test/resources/testng.xml
-curl -o tests-integration/tests-backend/src/test/resources/testng.xml https://raw.githubusercontent.com/PasanT9/apim-test-integration/4.3.0-copy/testng.xml
-mvn clean install -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Ptestgrid -DskipBenchMarkTest=true -Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false
+#log_info "install pack into local maven Repository"
+#mvn install:install-file -Dfile=/opt/testgrid/workspace/product-apim/modules/distribution/product/target/wso2am-4.3.0-SNAPSHOT.zip -DgroupId=org.wso2.am -DartifactId=wso2am -Dversion=4.3.0-SNAPSHOT -Dpackaging=zip
+#cd $INT_TEST_MODULE_DIR
+#rm -rf tests-integration/tests-backend/src/test/resources/testng.xml
+#curl -o tests-integration/tests-backend/src/test/resources/testng.xml https://raw.githubusercontent.com/PasanT9/apim-test-integration/4.3.0-copy/testng.xml
+#mvn clean install -fae -B -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -Ptestgrid -DskipBenchMarkTest=true -Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false
